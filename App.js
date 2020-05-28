3896ff1ae1544c42afbe3b42642bf35e34469cb3
sarafotros <sarafotros@gmail.com>
2020-05-28 18:09:23 +0100

    secondround

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+ 
   container: {
     flex: 1,
     backgroundColor: '#fff',
