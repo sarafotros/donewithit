e8725afd5e6f618c8e3b7850b323852760f4bb7d
sarafotros <sarafotros@gmail.com>
2020-06-07 22:56:15 +0100

    yup validation

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import { StyleSheet, Image, View } from 'react-native';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
+import ErrorMessage from '../components/ErrorMessage';
+
+const validationSchema = Yup.object().shape({
+	email: Yup.string().required().email().label('Email'),
+	password: Yup.string().required().min(4).label('Password'),
+});
 
 export default function LoginScreen() {
 	// const [email, setEmail] = useState();
@@ -16,31 +24,33 @@ export default function LoginScreen() {
 			<Formik
 				initialValues={{ email: '', password: '' }}
 				onSubmit={(value) => console.log(value)}
+				validationSchema={validationSchema}
 			>
-				{({ handleChange, handleSubmit }) => (
+				{({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
 					<>
 						<AppTextInput
 							icon="email"
 							placeholder="Email"
 							autoCapitalize="none"
 							autoCorrect={false}
+							onBlur={() => setFieldTouched('email')}
 							onChangeText={handleChange('email')}
 							keyboardType="email-address"
 							textContentType="emailAddress"
 						/>
+						<ErrorMessage error={errors.email} visible={touched.email} />
 						<AppTextInput
 							autoCapitalize="none"
 							autoCorrect={false}
 							placeholder="Password"
 							icon="lock"
 							secureTextEntry
+							onBlur={() => setFieldTouched('password')}
 							textContentType="password"
 							onChangeText={handleChange('password')}
 						/>
-						<AppButton
-							title="Login"
-							onPress={handleSubmit}
-						/>
+						<ErrorMessage error={errors.password} visible={touched.password} />
+						<AppButton title="Login" onPress={handleSubmit} />
 					</>
 				)}
 			</Formik>
@@ -49,9 +59,9 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10
-    },
+	container: {
+		padding: 10,
+	},
 	logo: {
 		width: 80,
 		height: 80,
