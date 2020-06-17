411e25d6fa60a3b326b71e90dad20c99fa3867c0
sarafotros <sarafotros@gmail.com>
2020-06-17 23:21:19 +0100

    exppo permission

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import AccountScreen from './app/screens/AccountScreen';
@@ -7,6 +7,8 @@ import Screen from './app/components/Screen';
 export default function App() {
 	const requestPermission = async () => {
 		const result = await ImagePicker.requestCameraRollPermissionsAsync();
+		if (!result.granted)
+			alert('You need to enable permission to access the library');
 	};
 
 	useEffect(() => {
