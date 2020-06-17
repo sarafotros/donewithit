7593e6d26d3f35e410be0674fb5598e170117ca3
sarafotros <sarafotros@gmail.com>
2020-06-17 22:46:47 +0100

    image picker

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import * as ImagePicker from 'expo-image-picker';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import AccountScreen from './app/screens/AccountScreen';
+import Screen from './app/components/Screen';
 
 export default function App() {
-	return <AccountScreen />;
+	const requestPermission = async () => {
+		const result = await ImagePicker.requestCameraRollPermissionsAsync();
+	};
+
+	useEffect(() => {
+		requestPermission();
+	}, []);
+
+	return <Screen></Screen>;
 }
